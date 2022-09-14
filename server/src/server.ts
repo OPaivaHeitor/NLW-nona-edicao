f51d13cf535aa5fe8292dea6900bd31b4be67e66
Heitor <heitorpaiva2012@gmail.com>
2022-09-13 23:44:59 -0300

    Primeiro commit

---
+++ server/src/server.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+
+const app = express()
+const prisma = new PrismaClient({
+    log: ['query']
+})
+
+app.get('/games', async (request, response) => {
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads: true,
+                }
+            }
+        }
+    })
+
+
+    return response.json(games);
+});
+
+app.post('/ads', (request, response) => {
+    return response.json(['asd']);
+});
+
+
+app.get('/games/:id/ads', (request, response) => {
+    // const gameId = request.params.id;
+
+    return response.json([
+        { id: 1, name: 'Anúncio 1' },
+        { id: 2, name: 'Anúncio 2' },
+        { id: 3, name: 'Anúncio 3' },
+        { id: 4, name: 'Anúncio 4' },
+        { id: 5, name: 'Anúncio 5' },
+    ])
+})
+
+
+app.get('/games/:id/discord', (request, response) => {
+    // const adId = request.params.id;
+
+    return response.json([])
+})
+
+app.listen(3333)
